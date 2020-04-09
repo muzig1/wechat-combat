95416411f19aa6078f89ffb2da09e8a0f3ee4dc9
ligang <ligang@nibirutech.com>
2020-04-09 22:27:22 +0800

    [bugfix]  修复js数据导出问题

---
+++ data/post_data.js
@@ -0,0 +1,24 @@
+var data = [
+  {
+    avatar_img: "/images/avatar/1.png",
+    date: "Nov 20 2020",
+    post_img: "/images/post/crab.png",
+    content:
+      "一叶落便知秋意浓，即使江南的绿色褪色之期晚了几许，南飞的大雁也会在天空一会儿排成一字，一会儿排成人字，秋天真的来了，中秋真的来了，国庆真的来了。\n一叶落便知秋意浓，即使江南的绿色褪色之期晚了几许，南飞的大雁也会在天空一会儿排成一字，一会儿排成人字，秋天真的来了，中秋真的来了，国庆真的来了。",
+    collection_num: 79,
+    view_num: 200,
+  },
+  {
+    avatar_img: "/images/avatar/1.png",
+    date: "Nov 20 2020",
+    post_img: "/images/post/crab.png",
+    content:
+      "一叶落便知秋意浓，即使江南的绿色褪色之期晚了几许，南飞的大雁也会在天空一会儿排成一字，一会儿排成人字，秋天真的来了，中秋真的来了，国庆真的来了。\n一叶落便知秋意浓，即使江南的绿色褪色之期晚了几许，南飞的大雁也会在天空一会儿排成一字，一会儿排成人字，秋天真的来了，中秋真的来了，国庆真的来了。",
+    collection_num: 79,
+    view_num: 200,
+  },
+];
+
+module.exports = {
+  data: data,
+};
--- pages/posts/pages.js
+++ pages/posts/pages.js
@@ -1,3 +1,5 @@
+var post_data = require("../../data/post_data.js");
+
 // pages/posts/pages.js
 Page({
   /**
@@ -11,30 +13,8 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-    var content = [
-      {
-        avatar_img: "/images/avatar/1.png",
-        date: "Nov 20 2020",
-        post_img: "/images/post/crab.png",
-        content:
-          "一叶落便知秋意浓，即使江南的绿色褪色之期晚了几许，南飞的大雁也会在天空一会儿排成一字，一会儿排成人字，秋天真的来了，中秋真的来了，国庆真的来了。\n一叶落便知秋意浓，即使江南的绿色褪色之期晚了几许，南飞的大雁也会在天空一会儿排成一字，一会儿排成人字，秋天真的来了，中秋真的来了，国庆真的来了。",
-        collection_num: 79,
-        view_num: 200,
-        post_img_condition: true,
-      },
-      {
-        avatar_img: "/images/avatar/1.png",
-        date: "Nov 20 2020",
-        post_img: "/images/post/crab.png",
-        content:
-          "一叶落便知秋意浓，即使江南的绿色褪色之期晚了几许，南飞的大雁也会在天空一会儿排成一字，一会儿排成人字，秋天真的来了，中秋真的来了，国庆真的来了。\n一叶落便知秋意浓，即使江南的绿色褪色之期晚了几许，南飞的大雁也会在天空一会儿排成一字，一会儿排成人字，秋天真的来了，中秋真的来了，国庆真的来了。",
-        collection_num: 79,
-        view_num: 200,
-        post_img_condition: true,
-      },
-    ];
     this.setData({
-      content: content,
+      content: post_data.data,
     });
   },
 

f2d3ca3b85653244121c05502ca1b2f872c85422
ligang <ligang@nibirutech.com>
2020-04-14 08:45:21 +0800

    使用缓存, 并实现收藏按钮

--- pages/posts/post-detail/detail.js
+++ pages/posts/post-detail/detail.js
@@ -5,5 +5,32 @@ Page({
     var id = args.id;
     var data = postData.data[id];
     this.setData({ ...data });
+    this.setData({
+      currentIndex: id,
+    });
+
+    // 更新缓存
+    var collection = wx.getStorageSync("collection");
+    if (collection) {
+      var col = collection[id];
+      this.setData({ collection: col });
+    } else {
+      var col = {};
+      col[id] = true;
+      wx.setStorageSync("collection", col);
+      wx.setData({ collection: col[id] });
+    }
   },
+
+  onCollectionTap: function (event) {
+    console.log("ss");
+    var col = wx.getStorageSync("collection");
+    var collect = col[this.data.currentIndex];
+    collect = !collect;
+    col[this.data.currentIndex] = collect;
+    wx.setStorageSync("collection", col);
+    this.setData({ collection: collect });
+  },
+
+  onShareTap: function (event) {},
 });

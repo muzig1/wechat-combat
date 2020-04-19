478420d124fa4e941ef616d1185d74912fba328c
ligang <ligang@nibirutech.com>
2020-04-19 16:42:34 +0800

    新增tabBar

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -3,7 +3,14 @@ Page({
     // wx.navigateTo({
     //   url: "/pages/posts/pages",
     // });
-    wx.redirectTo({
+    // wx.redirectTo({
+    //   url: "/pages/posts/pages",
+    //   success: (result) => {},
+    //   fail: () => {},
+    //   complete: () => {},
+    // });
+
+    wx.switchTab({
       url: "/pages/posts/pages",
       success: (result) => {},
       fail: () => {},

46cb58b05531a36a0b2a58b6ddf217333330b970
ligang <ligang@nibirutech.com>
2020-05-14 08:59:45 +0800

    [电影详情] 初步完成封面和介绍

---
+++ pages/movies/item-detail/item-detail.js
@@ -0,0 +1,74 @@
+// pages/movies/item-detail/item-detail.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    cover_img:"/images/bill/jiuzhou.webp",
+    like_num:1233,
+    comment_num:123,
+    elem:{
+      views:300,
+      direct: "刘振伟",
+      filmmakers: ["周星驰", "吴孟达", "朱茵", "蔡少芬"],
+      type: ["动作", "冒险", "喜剧"]
+    }
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
